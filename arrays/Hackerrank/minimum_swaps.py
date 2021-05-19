7e0e3200ef675dbec0e7d9e8f12fe9e70ed0c9fe
Psycadelik <francisadrian9@hotmail.com>
2021-05-19 22:11:28 +0300

    :up: update minimum swaps solution

--- arrays/Hackerrank/minimum_swaps.py
+++ arrays/Hackerrank/minimum_swaps.py
@@ -4,10 +4,26 @@ import sys
 def minimumSwaps(arr):
     print(arr)
     count = 0
-    for i in range(len(arr)):
-        if i > i+1:
-    
+    # loop through the array
+    for i in range(0, len(arr)-1):
+        print("current index value", arr[i])
+        print("next index value", arr[i+1])
+        
+        # check if current index value is greater than the next index value
+        if arr[i] > arr[i+1]:
+            # assign the value to a temp variable
+            temp = arr[i]
+            # swap the value in the next index to current index
+            arr[i] = arr[i+1]
+            # assign the value stored in temp to the next index
+            arr[i+1] = temp
+            # increase counter
+            count += 1
 
+    print("final count", count)        
+    return count
+
+            
 def test(did_pass):
     if did_pass:
         response = "Test Ok"
@@ -21,3 +37,4 @@ def test_suite():
     test(minimumSwaps([4, 3, 1, 2]) == 4)
 
 test_suite()
+
