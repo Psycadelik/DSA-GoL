2dcc9c64a7f4e1210d39174396961e0750824d78
Psycadelik <francisadrian9@hotmail.com>
2021-05-22 21:24:48 +0300

    :ticket: add remove element leetcode solution

--- arrays/Leetcode/remove_element.py
+++ arrays/Leetcode/remove_element.py
@@ -1,11 +1,17 @@
 def remove_element(nums, val):
+    k = 0
+    lens = 0
     # create an empty array
     vals = []
 
     for i in range(len(nums)):
-        if nums[i] == val:
-            vals.append(nums[i])
-    print(len(nums))
+        if nums[i-k] == val:
+            nums.append(val)
+            nums.pop(i-k)
+            k += 1
+        else:
+            lens += 1
+    return lens
 
 def test(did_pass):
     if did_pass:
@@ -15,7 +21,7 @@ def test(did_pass):
     print(res)
 
 def test_suite():
-    test(remove_element([3,2,2,3], 2) == 2)
+    test(remove_element([3,2,2,3], 3) == 2)
 
 test_suite()
 
